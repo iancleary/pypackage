ef6c0cd02ade0fe12caf4c352dbf2d570474175d
Ian Cleary <iancleary@pm.me>
2019-10-24 19:28:58 -0700

    initial testing setup

---
+++ test.sh
@@ -0,0 +1,15 @@
+#! /usr/bin/env bash
+
+# Exit in case of error
+set -e
+
+rm -rf ./samplemod
+
+cookiecutter --config-file ./testing-config.yml --no-input -f ./
+
+cd ./samplemod
+
+pipenv install --dev
+pipenv run bash ./scripts/test.sh
+
+cd ../
