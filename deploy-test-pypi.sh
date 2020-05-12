b1c260403a9470461542c4ad4fb1e48d26818658
iancleary <iancleary@pm.me>
2020-05-11 22:24:05 -0700

    Debugging

--- deploy-test-pypi.sh
+++ deploy-test-pypi.sh
@@ -4,9 +4,16 @@
 set -e
 
 # Setup to use token (do this in the actual package)
+echo "poetry config pypi-token.pypi my-token"
 poetry config pypi-token.pypi my-token
 # Setup to use test Pypi and not real pypi (don't do this in the actual package)
+echo "poetry config repositories.pypi https://test.pypi.org/simple/"
 poetry config repositories.pypi https://test.pypi.org/simple/
 
 # Run deploy script
-bash scripts/deploy.sh
+echo "cookiecutter --config-file ./config-travis-testing.yml --no-input -f ./"
+cookiecutter --config-file ./config-travis-testing.yml --no-input -f ./
+ls
+cd ./pypackage
+ls
+bash ./scripts/publish.sh
